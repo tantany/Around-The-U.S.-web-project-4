fe8ba62c1cff3b5f43792298aade14c2d27dc8a3
Tanya Margulis <Tany999m@gmail.com>
2020-06-25 18:56:20 +0200

    feature: changed project file stracture and added project building with webpack plus correct css minification and autoprefixing

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // connect plugin
-
-// connect mini-css-extract-plugin to the project
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
@@ -17,32 +15,33 @@ module.exports = {
         loader: "babel-loader",
         exclude: "/node_modules/"
       },
-      // add the rule for processing files
       {
-        // this regular expression will search for files with the following extensions
-        test: /\.(png|svg|jpg|gif|woff2|woff)$/,
-        // file-loader should be used when processing those files
-        loader: "file-loader"
+        test: /\.css$/,
+        loader: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1
+            }
+          },
+          "postcss-loader"
+        ],
       },
-      // add the rule for working with HTML in a similar way
       {
         test: /\.html$/,
         loader: "html-loader",
       },
-      // add another file:
       {
-        // use these rules only for CSS files
-          test: /\.css$/,
-        // use MiniCssExtractPlugin.loader и css-loader
-        // when processing these files
-          loader:  [MiniCssExtractPlugin.loader, "css-loader"]
-      }
+        test: /\.(png|svg|jpg|gif|woff)$/,
+        loader: "file-loader",
+      },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html"
     }),
-      new MiniCssExtractPlugin() // connect the plugin for merging CSS files
+    new MiniCssExtractPlugin()
   ]
 };
